fd0edfb1b2dde94c05a8ff508ab254dfe7ce1445
ecrseer <juustino@live.com>
2022-09-09 21:14:38 -0300

    rds connection

--- src/main/java/br/infnet/edu/gjventuravagasbackend/gjventuravagasbackend/domain/Usuario.java
+++ src/main/java/br/infnet/edu/gjventuravagasbackend/gjventuravagasbackend/domain/Usuario.java
@@ -1,18 +1,29 @@
 package br.infnet.edu.gjventuravagasbackend.gjventuravagasbackend.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity(name = "usuario")
-public class Usuario {
+public class Usuario implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long idUsuario;
 
     private String login;
     private String password;
+
 
     @Override
     public String toString() {
@@ -23,37 +34,5 @@ public class Usuario {
                 '}';
     }
 
-    public Usuario() {
-    }
-
-    public long getIdUsuario() {
-        return idUsuario;
-    }
-
-    public void setIdUsuario(long idUsuario) {
-        this.idUsuario = idUsuario;
-    }
-
-    public Usuario(long idUsuario, String login, String password) {
-        this.setIdUsuario(idUsuario);
-        this.login = login;
-        this.password = password;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 
 }
