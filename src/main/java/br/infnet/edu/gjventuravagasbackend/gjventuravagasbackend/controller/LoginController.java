1140a83a45fba1ae048acad4a35a8b2603d34759
ecrseer <juustino@live.com>
2022-09-14 20:15:32 -0300

    oauth rename

--- src/main/java/br/infnet/edu/gjventuravagasbackend/gjventuravagasbackend/controller/LoginController.java
+++ src/main/java/br/infnet/edu/gjventuravagasbackend/gjventuravagasbackend/controller/LoginController.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/login")
+@RequestMapping("/loginEmpresa")
 public class LoginController {
 
     @Autowired
